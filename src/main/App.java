2562780c77c5f722d48a2d02a25f2955cf49a23b
Erick Jonathan <zxjonathanxz22@gmail.com>
2023-10-20 19:18:28 -0300

    funções prontas

--- src/main/App.java
+++ src/main/App.java
@@ -1,15 +1,47 @@
 package main;
 public class App {
+
     public static int encontre(int[] arrayOrdenado, int num){
-        int a = 0;
-        return -1;
-    }
+        
+        for(int i = 0; i<arrayOrdenado.length; i++){
+            if(arrayOrdenado[i] == num){
+                return 1;
+            }    
+        }
 
+        return 0;
+    }
+    
     public static int maior(int[] arrayDesordenado){
-        return -1;
+        
+        int numero = arrayDesordenado[0];
+        
+        for(int i = 0; i<arrayDesordenado.length; i++){
+            if(arrayDesordenado[i] > numero ){
+                numero = arrayDesordenado[i];
+            }    
+        }
+    
+        return numero;
     }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
+    public static Double parMaisProximo(Integer[][] pares){ 
+
+        Double menorDistancia = Double.MAX_VALUE;  
+
+        for(int i = 0; i < pares.length - 1; i++){
+            for(int j = i+1; j < pares.length; j++){
+                Double eixo_x = (Double) Math.pow(pares[i][0] - pares[j][0], 2);
+                Double eixo_y = (Double) Math.pow(pares[i][1] - pares[j][1], 2);
+                Double distancia = (Double) Math.sqrt(eixo_x + eixo_y);
+                
+                if(distancia < menorDistancia){
+                    menorDistancia = distancia;   
+                }
+            
+            } 
+        }
+
+        return menorDistancia; 
     }
 }
